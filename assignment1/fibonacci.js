ae1733cccd93c1523d679b4bab8ba747d0de6996
Ivan Delgado <46804151+vndlgd@users.noreply.github.com>
2024-03-04 14:19:55 -0800

    Submit iterative fibonacci sequence solution

--- assignment1/fibonacci.js
+++ assignment1/fibonacci.js
@@ -1,6 +1,23 @@
 function fibs(n) {
   // write code here
+  if (n === 0) {
+    return [];
+  } else if (n === 1) {
+    return [0];
+  } else if (n === 2) {
+    return [0, 1];
+  }
+
+  let array = [0, 1];
+
+  // n - 2 to account for already having the first two fib numbers
+  for (let i = 0; i < n - 2; i++) {
+    array.push(array[i] + array[i + 1]);
+  }
+  return array;
 }
+
+console.log(fibs(8));
 
 function fibsRec(n) {
   // write code here
