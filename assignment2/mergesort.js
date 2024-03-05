65be316a1ec88d4307d7b37680e201ca9de7cf5c
Ivan Delgado <46804151+vndlgd@users.noreply.github.com>
2024-03-04 20:44:18 -0800

    Submit merge sort solution

--- assignment2/mergesort.js
+++ assignment2/mergesort.js
@@ -1,7 +1,34 @@
 function mergeSort(array) {
-  // write code here
+  if (array.length < 2) {
+    return array;
+  }
+
+  const mid = Math.floor(array.length / 2);
+  const left = mergeSort(array.slice(0, mid));
+  const right = mergeSort(array.slice(mid, array.length));
+  return merge(left, right);
 }
 
 function merge(left, right) {
-  // write code here
+  let mergedArray = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < left.length && j < right.length) {
+    if (left[i] < right[j]) {
+      mergedArray.push(left[i]);
+      i++;
+    } else {
+      mergedArray.push(right[j]);
+      j++;
+    }
+  }
+
+  // copy the remaining elements
+  mergedArray = mergedArray.concat(left.slice(i)).concat(right.slice(j));
+
+  return mergedArray;
 }
+
+console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1]));
+console.log(mergeSort([105, 79, 100, 110]));
